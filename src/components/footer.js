6ef606c1be130be16a98b475c923121b54982a99
Tiago Pina <tiago.pina@ua.pt>
2025-04-15 10:55:32 +0100

    Melhorias no footer

--- src/components/footer.js
+++ src/components/footer.js
@@ -5,7 +5,7 @@ import hml from "../imgs/depvertical.png";
 import ue from "../imgs/ue.png";
 import rp from "../imgs/rp.png";
 import fct from "../imgs/fct.png";
-import pessoas30 from "../imgs/pessoas30.jpg"; //TENTAR ENCONTRAR ESTE LOGO SEM FUNDO OU RETIRAR NÓS O FUNDO
+import pessoas30 from "../imgs/pessoas30.png";
 import portugal30 from "../imgs/portugal30.png";
 
 const Footer = () => {
@@ -14,24 +14,24 @@ const Footer = () => {
       <div className="container">
         <div className="row g-4">
           {/* Promotores */}
-          <div className="col-md-6">
-            <h5 className="fw-semibold mb-3 text-uppercase" style={{ color: "#234970" }}>Promotores</h5>
+          <div className="col-md-5">
+            <h5 className="fw-semibold mb-4 text-uppercase" style={{ color: "#234970" }}>Promotores</h5>
             <div className="d-flex flex-wrap align-items-center gap-4">
-              <img src={dep} alt="União Europeia" className="img-fluid" style={{ maxHeight: "45px" }} />
-              <img src={wjcr} alt="William James Center for Research" className="img-fluid" style={{ maxHeight: "45px" }} />
-              <img src={hml} alt="Healthy Minds Lab" className="img-fluid" style={{ maxHeight: "45px" }} />
+              <img src={dep} alt="União Europeia" className="img-fluid" style={{ maxHeight: "55px" }} />
+              <img src={wjcr} alt="William James Center for Research" className="img-fluid" style={{ maxHeight: "55px" }} />
+              <img src={hml} alt="Healthy Minds Lab" className="img-fluid" style={{ maxHeight: "55px" }} />
             </div>
           </div>
 
           {/* Financiadores */}
-          <div className="col-md-6">
-            <h5 className="fw-semibold mb-3 text-uppercase" style={{ color: "#234970" }}>Financiadores</h5>
+          <div className="col-md-7">
+            <h5 className="fw-semibold mb-1 text-uppercase" style={{ color: "#234970" }}>Financiadores</h5>
             <div className="d-flex flex-wrap align-items-center gap-4">
-              <img src={ue} alt="União Europeia" className="img-fluid" style={{ maxHeight: "45px" }} />
-              <img src={rp} alt="República Portuguesa" className="img-fluid" style={{ maxHeight: "45px" }} />
-              <img src={fct} alt="FCT" className="img-fluid" style={{ maxHeight: "45px" }} />
-              <img src={pessoas30} alt="Pessoas 2030" className="img-fluid" style={{ maxHeight: "45px" }} />
-              <img src={portugal30} alt="Portugal 2030" className="img-fluid" style={{ maxHeight: "45px " }} />
+              <img src={ue} alt="União Europeia" className="img-fluid" style={{ maxHeight: "60px" }} />
+              <img src={rp} alt="República Portuguesa" className="img-fluid" style={{ maxHeight: "60px" }} />
+              <img src={fct} alt="FCT" className="img-fluid" style={{ maxHeight: "90px" }} />
+              <img src={pessoas30} alt="Pessoas 2030" className="img-fluid" style={{ maxHeight: "40px" }} />
+              <img src={portugal30} alt="Portugal 2030" className="img-fluid" style={{ maxHeight: "40px " }} />
             </div>
           </div>
         </div>
