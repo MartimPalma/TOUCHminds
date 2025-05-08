89b706a15506cf41b0d6097d8fbf8f9babea19af
martimpalma <martimpalma@ua.pt>
2025-05-08 16:19:46 +0100

    alteração card modulos landing

--- src/components/moduleCardLanding.js
+++ src/components/moduleCardLanding.js
@@ -2,18 +2,17 @@ import React from "react";
 
 const ModuleCard = ({ image, title, subtitle, description }) => {
   return (
-    <div className="col-md-4">
-      <div className="module-card">
-        <div className="card-inner">
-          <div className="card-front">
-            <img src={image} alt={title} className="card-image" />
-            <h5 className="card-title">{title}</h5>
-            <p className="card-subtitle">{subtitle}</p>
-          </div>
-          <div className="card-back">
-            <img src={image} alt={title} className="card-image" />
-            <p className="card-description">{description}</p>
-          </div>
+    <div className="col-md-4 mb-4">
+      <div className="module-card position-relative overflow-hidden">
+        <img src={image} alt={title} className="card-image w-100" />
+        
+        <div className="overlay-text position-absolute text-white text-start px-3 pb-3">
+          <h5 className="fw-bold">{title}</h5>
+          <p className="mb-2">{subtitle}</p>
+        </div>
+
+        <div className="overlay-description position-absolute top-0 start-0 w-100 h-100 d-flex flex-column justify-content-center align-items-center text-white text-center px-4">
+          <p>{description}</p>
         </div>
       </div>
     </div>
