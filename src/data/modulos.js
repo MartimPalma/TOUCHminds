c611191c74540c0e7301b29f4ae2c2fda98df076
martimpalma <martimpalma@ua.pt>
2025-05-30 16:57:27 +0100

    alteração data dos modulos

--- src/data/modulos.js
+++ src/data/modulos.js
@@ -8,10 +8,6 @@ const modulos = [
       imagem: "../imgs/module1.jpg",
       titulo: "Módulo 1",
       subtitulo: "Ansiedade NÃO é bicho-papão!",
-      descricao: "Aprende a diferenciar entre uma ansiedade comum e uma ansiedade SOS",
-      introducao: `Todos nós sentimos ansiedade – e isso é perfeitamente normal! Aqui, vais compreender melhor as emoções e como elas influenciam o nosso dia a dia.
-
-      Além disso, vais aprender a distinguir entre uma ansiedade comum e uma ansiedade SOS que causa sofrimento.`,
       atividades: [
         {
           url: "banda-desenhada",
@@ -75,8 +71,6 @@ const modulos = [
         imagem: "../imgs/module2.jpg",
         titulo: "Módulo 2",
         subtitulo: "Desmitificar a Ansiedade ",
-        descricao: "Nem tudo o que dizem sobre a ansiedade é verdade!",
-        introducao: "Todos nós sentimos ansiedade – e isso é perfeitamente normal! Aqui, vais compreender melhor as emoções, como elas influenciam o nosso dia a dia e aprender a distinguir entre uma ansiedade comum e uma ansiedade SOS que causa sofrimento.",
         atividades: [
           {
             url: "podcast",
@@ -106,33 +100,31 @@ const modulos = [
         imagem: "../imgs/module3.jpg",
         titulo: "Módulo 3",
         subtitulo: "Sê amigo de ti mesmo! ",
-        descricao: "O que dizes a ti próprio faz a diferença!",
-        introducao: "Todos nós sentimos ansiedade – e isso é perfeitamente normal! Aqui, vais compreender melhor as emoções, como elas influenciam o nosso dia a dia e aprender a distinguir entre uma ansiedade comum e uma ansiedade SOS que causa sofrimento.",
         atividades: [
           {
-            url: "banda-desenhada",
-            titulo: "Ansiedade: Aliada ou Empecilho?",
+            url: "voz-critica-compassiva",
+            titulo: "A Voz Crítica E A Voz Compassiva",
             tipo: "texto",
             descricao: "Banda desenhada que explica ansiedade comum vs. SOS.",
             imagem: "../imgs/module1.jpg"
           },
           {
-            url: "linha-ansiedade",
-            titulo: "Linha da Ansiedade",
+            url: "nosso-maior-inimigo",
+            titulo: "O Nosso Maior Inimigo",
             tipo: "popup",
             descricao: "Explora ansiedade normativa vs. patológica numa situação.",
             imagem: "../imgs/module1.jpg"
           },
           {
-            url: "unindo-experiencias",
-            titulo: "Unindo Experiências",
+            url: "abracar-me-a-mim-mesmo",
+            titulo: "Abraçar-Me A Mim Mesmo/a",
             tipo: "video",
             descricao: "Vídeo com relatos reais sobre ansiedade.",
             imagem: "../imgs/module1.jpg"
           },
           {
-            url: "ansiedade-sem-filtros",
-            titulo: "Ansiedade sem Filtros",
+            url: "atividade-resumo",
+            titulo: "Atividade Resumo",
             tipo: "imagem",
             descricao: "Imagens estilo Instagram sobre ansiedade na adolescência.",
             imagem: "../imgs/module1.jpg"
@@ -144,30 +136,28 @@ const modulos = [
         imagem: "../imgs/module4.jpg",
         titulo: "Módulo 4",
         subtitulo: "O Poder da Mudança!",
-        descricao: "A mudança faz parte da vida! Explora os diferentes estádios da mudança, aprende a definir objetivos realistas e descobre como dar pequenos passos pode fazer uma grande diferença.",
-        introducao: "Todos nós sentimos ansiedade – e isso é perfeitamente normal! Aqui, vais compreender melhor as emoções, como elas influenciam o nosso dia a dia e aprender a distinguir entre uma ansiedade comum e uma ansiedade SOS que causa sofrimento.",
         atividades: [
           {
-            url: "banda-desenhada",
-            titulo: "Ansiedade: Aliada ou Empecilho?",
+            url: "ferramentas-mudanca",
+            titulo: "Ferramentas De Mudança",
             descricao: "Banda desenhada que explica ansiedade comum vs. SOS.",
             imagem: "../imgs/module1.jpg"
           },
           {
-            url: "linha-ansiedade",
-            titulo: "Linha da Ansiedade",
+            url: "balanca-virtual",
+            titulo: "Balança Virtual",
             descricao: "Explora ansiedade normativa vs. patológica numa situação.",
             imagem: "../imgs/module1.jpg"
           },
           {
-            url: "unindo-experiencias",
-            titulo: "Unindo Experiências",
+            url: "ja-foste-capaz",
+            titulo: "Já Foste Capaz!",
             descricao: "Vídeo com relatos reais sobre ansiedade.",
             imagem: "../imgs/module1.jpg"
           },
           {
-            url: "ansiedade-sem-filtros",
-            titulo: "Ansiedade sem Filtros",
+            url: "atividade-resumo",
+            titulo: "Atividade Resumo",
             descricao: "Imagens estilo Instagram sobre ansiedade na adolescência.",
             imagem: "../imgs/module1.jpg"
           },
@@ -178,8 +168,6 @@ const modulos = [
         imagem: "../imgs/module5.jpg",
         titulo: "Módulo 5",
         subtitulo: "Reviravolta em Rede!",
-        descricao: "Pedir ajuda pode parecer difícil, mas não estás sozinho. Aprende a identificar sinais de alerta, a diferenciar entre ajuda formal e informal e descobre como a ajuda das pessoas mais próximas podem fazer toda a diferença.",
-        introducao: "Todos nós sentimos ansiedade – e isso é perfeitamente normal! Aqui, vais compreender melhor as emoções, como elas influenciam o nosso dia a dia e aprender a distinguir entre uma ansiedade comum e uma ansiedade SOS que causa sofrimento.",
         atividades: [
           {
             url: "banda-desenhada",
@@ -216,8 +204,6 @@ const modulos = [
         imagem: "../imgs/module6.jpg",
         titulo: "Módulo 6",
         subtitulo: "Um novo Começo!",
-        descricao: "Como funciona a ajuda profissional? Descobre os sinais que indicam que podes precisar de ajuda profissional, conhece o papel do psicólogo e explora os recursos disponíveis para dares o próximo passo.",
-        introducao: "Todos nós sentimos ansiedade – e isso é perfeitamente normal! Aqui, vais compreender melhor as emoções, como elas influenciam o nosso dia a dia e aprender a distinguir entre uma ansiedade comum e uma ansiedade SOS que causa sofrimento.",
         atividades: [
           {
             url: "banda-desenhada",
